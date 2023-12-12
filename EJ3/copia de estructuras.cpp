76d73b862ef072676f348f857b32204bd5a1ee08
ZoeAcero <zar.amazona@gmail.com>
2023-12-12 13:05:30 +0100

    6

--- EJ3/copia de estructuras.cpp
+++ EJ3/copia de estructuras.cpp
@@ -8,3 +8,10 @@ struct Estudiante {
     float promedio;
 };
 
+struct Estudiante copiarEstudiante(const struct Estudiante *original) {
+    struct Estudiante copia;
+    strcpy(copia.nombre, original->nombre);
+    copia.edad = original->edad;
+    copia.promedio = original->promedio;
+    return copia;
+}
