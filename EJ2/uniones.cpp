49447279924f373953f3fc2729753ddcd24c2f3b
ZoeAcero <zar.amazona@gmail.com>
2023-12-12 12:23:59 +0100

    5

--- EJ2/uniones.cpp
+++ EJ2/uniones.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 union Dato {
     int i;
@@ -7,5 +8,16 @@ union Dato {
 };
 
 int main(){
-    
+    union Dato dato;
+
+    dato.i = 10;
+    printf("dato.i: %d\n", dato.i);
+
+    dato.f = 220.5;
+    printf("dato.f: %.2f\n", dato.f);
+
+    strcpy(dato.str, "C programming");
+    printf("dato.str: %s\n", dato.str);
+
+    return 0;
 }
