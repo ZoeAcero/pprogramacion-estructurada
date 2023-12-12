3baff7276fbc877644bf430fd077b5e465468ffd
ZoeAcero <zar.amazona@gmail.com>
2023-12-12 13:27:44 +0100

    15

--- EJ5/estructuras y funciones.cpp
+++ EJ5/estructuras y funciones.cpp
@@ -5,6 +5,10 @@ struct Estudiante {
     char nombre[50];
     int edad;
     float promedio;
+
+    void imprimirInformacion() {
+
+    }
 };
 
 void imprimirEstudiantePorValor(struct Estudiante estudiante) {
@@ -25,12 +29,34 @@ void imprimirEstudiantePorDireccion(const struct Estudiante *estudiante) {
     printf("Promedio: %.2f\n", estudiante->promedio);
 }
 
+class Estudiante {
+public:
+
+   (std::string nombre, int edad, float promedio)
+            : nombre(nombre), edad(edad), promedio(promedio) {}
+
+
+    void imprimirInformacion() const {
+        std::cout << "Nombre: " << nombre << "\n";
+        std::cout << "Edad: " << edad << "\n";
+        std::cout << "Promedio: " << promedio << "\n";
+    }
+
+private:
+    std::string nombre;
+    int edad;
+    float promedio;
+};
+
 int main(){
 
     struct Estudiante estudiante = {"Juan", 20, 85.5};
     imprimirEstudiantePorValor(estudiante);
     imprimirEstudiantePorReferencia(&estudiante);
     imprimirEstudiantePorDireccion(&estudiante);
+
+    estudiante.imprimirInformacion();
+
 
     return 0;
 
