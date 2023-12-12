b1b0842e69d76cacb87cac29db114d5bb2ee1584
ZoeAcero <zar.amazona@gmail.com>
2023-12-12 12:13:48 +0100

    2

--- EJ1/estructuras.cpp
+++ EJ1/estructuras.cpp
@@ -8,3 +8,31 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+void ingresarDatos(struct Estudiante *estudiante) {
+    printf("Ingrese el nombre del estudiante: ");
+    scanf("%s", estudiante->nombre);
+
+    printf("Ingrese la edad del estudiante: ");
+    scanf("%d", &estudiante->edad);
+
+    printf("Ingrese el promedio del estudiante: ");
+    scanf("%f", &estudiante->promedio);
+}
+
+void mostrarInformacion(const struct Estudiante *estudiante) {
+    printf("\nInformación del estudiante:\n");
+    printf("Nombre: %s\n", estudiante->nombre);
+    printf("Edad: %d\n", estudiante->edad);
+    printf("Promedio: %.2f\n", estudiante->promedio);
+}
+
+float calcularPromedioGeneral(const struct Estudiante estudiantes[], int numEstudiantes) {
+    float suma = 0.0;
+
+    for (int i = 0; i < numEstudiantes; ++i) {
+        suma += estudiantes[i].promedio;
+    }
+
+    return (numEstudiantes > 0) ? suma / numEstudiantes : 0.0;
+}
