560dbc7c7083a285cbcd119aa9ff52dbc5d7cea9
ZoeAcero <zar.amazona@gmail.com>
2023-12-12 13:11:54 +0100

    10

--- EJ4/alias.cpp
+++ EJ4/alias.cpp
@@ -17,3 +17,24 @@ Estudiante copiarEstudiante(const Estudiante *original) {
     return copia;
 }
 
+int main(){
+
+    Estudiante{"Juan", 20, 85.5};
+    Estudiante estudianteOriginal = {"Juan", 20, 85.5};
+
+    printf("Datos originales:\n");
+    printf("Nombre: %s\n", estudianteOriginal.nombre);
+    printf("Edad: %d\n", estudianteOriginal.edad);
+    printf("Promedio: %.2f\n", estudianteOriginal.promedio);
+
+    printf("\nDatos copiados:\n");
+    int estudianteCopia;
+    printf("Nombre: %s\n", estudianteCopia.nombre);
+    printf("Edad: %d\n", estudianteCopia.edad);
+    printf("Promedio: %.2f\n", estudianteCopia.promedio);
+
+
+
+
+    return 0;
+}
