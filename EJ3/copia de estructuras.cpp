ab17441d3e35b68a0d599d5e943638474bb2d01e
ZoeAcero <zar.amazona@gmail.com>
2023-12-12 13:06:59 +0100

    7

--- EJ3/copia de estructuras.cpp
+++ EJ3/copia de estructuras.cpp
@@ -15,3 +15,22 @@ struct Estudiante copiarEstudiante(const struct Estudiante *original) {
     copia.promedio = original->promedio;
     return copia;
 }
+
+int main(){
+
+    struct Estudiante estudianteOriginal = {"Juan", 20, 85.5};
+    struct Estudiante estudianteCopia = copiarEstudiante(&estudianteOriginal);
+
+    printf("Datos originales:\n");
+    printf("Nombre: %s\n", estudianteOriginal.nombre);
+    printf("Edad: %d\n", estudianteOriginal.edad);
+    printf("Promedio: %.2f\n", estudianteOriginal.promedio);
+
+    printf("\nDatos copiados:\n");
+    printf("Nombre: %s\n", estudianteCopia.nombre);
+    printf("Edad: %d\n", estudianteCopia.edad);
+    printf("Promedio: %.2f\n", estudianteCopia.promedio);
+
+
+    return 0;
+}
